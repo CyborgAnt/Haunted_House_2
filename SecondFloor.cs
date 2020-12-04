a32c2a5cf31de8829ab3165206870ba5bcefad74
Anthony Bonner <CyborgBrass@outlook.com>
2020-12-04 18:41:57 -0500

    Added code to the Second Floor Rooms; added frameworks for the halls that connect the rooms.

--- SecondFloor.cs
+++ SecondFloor.cs
@@ -1,7 +1,7 @@
 //second floor
 //intro code
 
-class SecondFloor
+class SecondFloorRooms
 {
     public static void stairs()
     {
@@ -15,15 +15,46 @@ class SecondFloor
         Console.ReadLine();
         
         WriteLine("Which way do you go?");
-        WriteLine("'1' to take the Left Hall and check the Closest Door\n'2' to take the Left Hall and check the Farthest Door\n'3' to take the Left Hall but skip both doors\n'4' to take the Right Hall and check the Closest Door\n'6' to take the Right Hall but Skip the Closest Door\n'7' to go back down the Stairs\n'0' to Quit the Program");
+        WriteLine("'1' to take the Left Hall and check the Closest Door\n'2' to take the Left Hall and check the Farthest Door\n'3' to take the Left Hall but skip both doors\n'4' to take the Right Hall and check the Closest Door\n'5' to take the Right Hall but Skip the Closest Door\n'6' to go back down the Stairs\n'7' to check the Door straight ahead\n'0' to Quit the Program");
         var upperStairsChoice = Int32.Parse(Console.ReadLine());
 
         switch (upperStairsChoice)
         {
-            
-
-
-            default:
+            case 1:
+                //Left - BTWall
+                behindTheWalls();
+                break;
+            case 2:
+                //Left - Bath2
+                bathroom2();
+                break;
+            case 3:
+                //Left - Hall
+                hallLeft();
+                break;
+            case 4:
+                //Right - BTWall
+                behindTheWalls();
+                break;
+            case 5:
+                //Right - Hall
+                hallRight();
+                break;
+            case 6:
+                //Stairs
+                WriteLine("You return to the Stairs. You descend back into the Living Room. Press 'Enter' to continue.");
+                Console.ReadLine();
+                HouseFirst.FrontHouse.livingRoom();
+                break;
+            case 7:
+                //Bedroom2 - puzzle/trap
+                WriteLine("Code coming soon!");
+                break;
+            case 0: default:
+                //exit code
+                WriteLine("You have exited the Program.");
+                System.Environment.Exit(0);
+                break;
         }
     }
 
@@ -72,12 +103,95 @@ class SecondFloor
     public static void bathroom2()
     {
         //nothing
-        WriteLine("Code coming soon!");
+        WriteLine("You turn down the Left Hall, skip the doors closest to you, and head to the door at the end of the Hall. Press 'Enter' to continue.");
+        Console.ReadLine();
+        Console.Clear();
+        WriteLine("You see a fairly large, relatively clean Bathroom. This was probably used as a Guest bathroom.\nYour standard bathtub, sink, shower head, and toilet are present but look practically spotless, compared to the rest of the house.\nThere are folded towels in a small closet, along with extra bath items like shampoo and body wash.\nNothing else seems out of the ordinary, so you leave the Bathroom and decide your next move. Press 'Enter' to continue.");
+        Console.ReadLine();
+        Console.Clear();
+        stairs();
     }
 
     public static void behindTheWalls()
     {
         //code on wall; wires; info about Orb, Locket
-        WriteLine("Code coming soon!");
+        WriteLine("You open the door and enter a narrow, U-shaped room.\nThis looks like a space where wires and cables that go between the floors of the house can run, without being seen.\nPress 'Enter' to continue.");
+        Console.ReadLine();
+        Console.Clear();
+
+        WriteLine("There are plenty of single and bundled wires, cords, and thicker, woven cables, running down (and into) the walls, and along the ceiling.\nThere are also, in regular intervals, small wooden shelves, set into the walls; on these shelves are various small tools, clamps, and fasteners. In a few spaces, there are also small work benches, with various tools, manuals, and papers on them.\This is a makeshift work shop.");
+        WriteLine("Press 'Enter' to continue.");
+        Console.ReadLine();
+        Console.Clear();
+
+        WriteLine("As you walk around the room, you notice on one of the smaller shelves a small, unlabeled, purple box.\nOn one of the larger work benches, a manual catches your eye: 'The Guardian Door'.\nYou have a few choices - what would you like to do?");
+        WriteLine("Press 'Enter' to continue.");
+        Console.ReadLine();
+
+        WriteLine("'1' to Inspect the Purple Box\n'2' to Check out the Manual\n'3' to Check the other Small Shelves\n'4' to Check the other Work Benches\n'5' to Check the Room\n'6' to Leave the Room through either Door\n'0' to Quit the Program");
+
+        var btwChoice = int32.Parse(Console.ReadLine());
+
+        switch (btwChoice)
+        {
+            case 1:
+                //Orb box
+                WriteLine("Code is coming!");
+                break;
+            case 2:
+                //Manual: Door needs Key to unlock, Orb and Locket to Activate Protection
+                WriteLine("Code is coming!");
+                break;
+            case 3:
+                //shelves - nothing
+                WriteLine("Code is coming!");
+                break;
+            case 4:
+                case 1:
+                //work benches - code on paper, drawings?
+                WriteLine("Code is coming!");
+                break;
+            case 5:
+                //notice glyphs on wall
+                WriteLine("Code is coming!");
+                break;
+            case 6:
+                //leave
+                WriteLine("You end your inspection of the U-shaped room and back to the Stairs.");
+                WriteLine("Press 'Enter' to continue.");
+                Console.ReadLine();
+                Console.Clear();
+                stairs();
+                break;       
+
+            case 0: default:
+                WriteLine("You have exited the Program.");
+                System.Environment.Exit(0); 
+
+        }
+
+
+    }
+}
+
+class secondFloorHalls
+{
+    public static void hallLeft()
+    {
+        //Hallway to the left of the floor
+        WriteLine("Code is coming!");
+        break;
+    }
+    public static void hallRight()
+    {
+        //Hallway to the right of the floor
+        WriteLine("Code is coming!");
+        break;
+    }
+    public static void hallBottom()
+    {
+        //Hallway to the bottom of the floor
+        WriteLine("Code is coming!");
+        break;
     }
 }
